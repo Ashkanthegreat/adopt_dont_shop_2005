5a5081826ad3f213605a6f24e07b743778501cf7
Ashkan Abbasi <airashkan@yahoo.com>
2020-07-03 23:45:44 -0600

    Finish US 4

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -13,4 +13,37 @@ RSpec.describe "Shelters Index Page" do
     expect(page).to have_content(@shelter1.name)
     expect(page).to have_content(@shelter2.name)
   end
+
+  it "see a link that takes you to a form to create a new shelter" do
+    visit "/shelters"
+
+    click_on "New Shelter"
+
+    expect(current_path).to eq('/shelters/new')
+
+    fill_in "name", with: "Puppy Love"
+    fill_in "address", with: "678 Jelly Bean Rd"
+    fill_in "city", with: "Denver"
+    fill_in "state", with: "CO"
+    fill_in "zip", with: "80210"
+
+    click_on "Create Shelter"
+
+    expect(current_path).to eq('/shelters')
+
+    expect(page).to have_content("Puppy Love")
+
+  end
 end
+
+# Then I am taken to '/shelters/new' where I  see a form for a new shelter
+# When I fill out the form with a new shelter's:
+# - name
+# - address
+# - city
+# - state
+# - zip
+# And I click the button "Create Shelter" to submit the form
+# Then a `POST` request is sent to '/shelters',
+# a new shelter is created,
+# and I am redirected to the Shelter Index page where I see the new Shelter listed.
