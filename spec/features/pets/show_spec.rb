60342ec2ba9cc93d72fa5fd9d5011813927e0e56
Ashkan Abbasi <airashkan@yahoo.com>
2020-07-05 18:41:57 -0600

    Create migration for pet status and route for show

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -2,8 +2,57 @@ require 'rails_helper'
 
 RSpec.describe "Pets Show Page" do
   before :each do
-    @shelter1 = Shelter.create!(name: "Pups For You", address: "1808 Pup lane", city: "Denver", state: "Colorado", zip: "80027")
-    @shelter2 = Shelter.create!(name: "Kitty Rescue", address: "1505 Kitty Dr.", city: "Denver", state: "Colorado", zip: "80025")
+    @shelter1 = Shelter.create!(name: "Pups For You",
+                                address: "1808 Pup lane",
+                                city: "Denver",
+                                state: "Colorado",
+                                zip: "80027")
+
+
+    @pet_1 = Pet.create(  image: "https://i.imgur.com/uQ7WVLL.jpg",
+                          name:  "Huxley",
+                          description: "Sweet senior dog",
+                          approximate_age: "18",
+                          sex: "Male",
+                          status: "adoptable",
+                          shelter_id: @shelter1.id)
+
+
+    @pet_2 = Pet.create(  image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2016/05/19091354/Weimaraner-puppy-outdoors-with-bright-blue-eyes.20190813165758508-1.jpg",
+                          name:  "Rocco",
+                          description: "Very good with kids and other animals",
+                          approximate_age: "6",
+                          sex: "Male",
+                          status: "adoptable",
+                          shelter_id: @shelter1.id)
   end
 
-end  
+  it "Can see a pets image, name, description, approximate age, sex, and adoptable status" do
+
+    visit "/pets/#{@pet_1.id}"
+
+    expect(page).to have_content(@pet_1.image)
+    expect(page).to have_content(@pet_1.name)
+    expect(page).to have_content(@pet_1.description)
+    expect(page).to have_content(@pet_1.approximate_age)
+    expect(page).to have_content(@pet_1.sex)
+    expect(page).to have_content(@pet_1.status)
+
+    expect(page).to_not have_content(@pet_2.name)
+
+
+  end
+
+end
+
+# User Story 9, Pet Show
+#
+# As a visitor
+# When I visit '/pets/:id'
+# Then I see the pet with that id including the pet's:
+# - image
+# - name
+# - description
+# - approximate age
+# - sex
+# - adoptable/pending adoption status
