3f7cfae4b3c9183d4b64168a5209d8f8b95f6b00
Ashkan Abbasi <airashkan@yahoo.com>
2020-07-05 21:46:06 -0600

    Make functionality to be able to edit a pet info from pet index page

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe "pets index page" do
                       approximate_age: "18",
                       sex: "Male",
                       shelter_id: shelter2.id)
-    visit "/pets"  
+    visit "/pets"
 
     expect(page).to have_content(pet1.name)
     expect(page).to have_content(pet1.image)
@@ -35,4 +35,40 @@ RSpec.describe "pets index page" do
 
     expect(page).to_not have_content(pet2.shelter.address)
   end
+
+  it "Can update a pet" do
+
+    shelter1 = Shelter.create!(name: "Pups For You", address: "1808 Pup lane", city: "Denver", state: "Colorado", zip: "80027")
+    shelter2 = Shelter.create!(name: "Pups For You2", address: "1808 Pup lane", city: "Denver", state: "Colorado", zip: "80027")
+
+    pet_1 = Pet.create!(  image: "https://i.imgur.com/uQ7WVLL.jpg",
+                      name:  "dog",
+                      description: "Sweet senior dog",
+                      approximate_age: "18",
+                      sex: "Male",
+                      shelter_id: shelter1.id)
+
+
+    visit "/pets"
+
+    click_on "Update Pet"
+
+    expect(current_path).to eq("/pets/#{pet_1.id}/edit")
+
+    fill_in "image", with: "https://dogtime.com/assets/uploads/2011/03/puppy-development-1280x720.jpg"
+    fill_in "name", with: "Happy"
+    fill_in "description", with: "Happy little puppy!"
+    fill_in "approximate_age", with: "<1"
+    fill_in "sex", with: "Male"
+
+    click_on "Update Pet"
+
+    expect(current_path).to eq("/pets/#{pet_1.id}")
+
+    expect(page).to have_content("Happy")
+    expect(page).to have_content("Happy little puppy!")
+
+    expect(page).to_not have_content("Huxley")
+  end
+
 end
--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -2,56 +2,83 @@ require 'rails_helper'
 
 RSpec.describe "Pets Show Page" do
   before :each do
-    @shelter1 = Shelter.create!(name: "Pups For You",
+  end
+
+  it "Can see a pets image, name, description, approximate age, sex, and adoptable status" do
+
+    shelter1 = Shelter.create!(name: "Pups For You",
                                 address: "1808 Pup lane",
                                 city: "Denver",
                                 state: "Colorado",
                                 zip: "80027")
 
 
-    @pet_1 = Pet.create(  image: "https://i.imgur.com/uQ7WVLL.jpg",
+    pet_1 = Pet.create(  image: "https://i.imgur.com/uQ7WVLL.jpg",
                           name:  "Huxley",
                           description: "Sweet senior dog",
                           approximate_age: "18",
                           sex: "Male",
                           status: "adoptable",
-                          shelter_id: @shelter1.id)
+                          shelter_id: shelter1.id)
 
 
-    @pet_2 = Pet.create(  image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2016/05/19091354/Weimaraner-puppy-outdoors-with-bright-blue-eyes.20190813165758508-1.jpg",
+    pet_2 = Pet.create(  image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2016/05/19091354/Weimaraner-puppy-outdoors-with-bright-blue-eyes.20190813165758508-1.jpg",
                           name:  "Rocco",
                           description: "Very good with kids and other animals",
                           approximate_age: "6",
                           sex: "Male",
                           status: "adoptable",
-                          shelter_id: @shelter1.id)
-  end
+                          shelter_id: shelter1.id)
 
-  it "Can see a pets image, name, description, approximate age, sex, and adoptable status" do
 
-    visit "/pets/#{@pet_1.id}"
+    visit "/pets/#{pet_1.id}"
 
-    expect(page).to have_content(@pet_1.image)
-    expect(page).to have_content(@pet_1.name)
-    expect(page).to have_content(@pet_1.description)
-    expect(page).to have_content(@pet_1.approximate_age)
-    expect(page).to have_content(@pet_1.sex)
-    expect(page).to have_content(@pet_1.status)
+    expect(page).to have_content(pet_1.image)
+    expect(page).to have_content(pet_1.name)
+    expect(page).to have_content(pet_1.description)
+    expect(page).to have_content(pet_1.approximate_age)
+    expect(page).to have_content(pet_1.sex)
+    expect(page).to have_content(pet_1.status)
 
-    expect(page).to_not have_content(@pet_2.name)
+    expect(page).to_not have_content(pet_2.name)
 
   end
 
   it "can delete a pet" do
 
-    visit "/pets/#{@pet_1.id}"
+    shelter1 = Shelter.create!(name: "Pups For You",
+                                address: "1808 Pup lane",
+                                city: "Denver",
+                                state: "Colorado",
+                                zip: "80027")
+
+
+    pet_1 = Pet.create(  image: "https://i.imgur.com/uQ7WVLL.jpg",
+                          name:  "Huxley",
+                          description: "Sweet senior dog",
+                          approximate_age: "18",
+                          sex: "Male",
+                          status: "adoptable",
+                          shelter_id: shelter1.id)
+
+
+    pet_2 = Pet.create(  image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2016/05/19091354/Weimaraner-puppy-outdoors-with-bright-blue-eyes.20190813165758508-1.jpg",
+                          name:  "Rocco",
+                          description: "Very good with kids and other animals",
+                          approximate_age: "6",
+                          sex: "Male",
+                          status: "adoptable",
+                          shelter_id: shelter1.id)
+
+
+    visit "/pets/#{pet_1.id}"
 
     click_on "Delete Pet"
 
     expect(current_path).to eq("/pets")
 
-    expect(page).to have_content(@pet_2.name)
-    expect(page).to_not have_content(@pet_1.name)
+    expect(page).to have_content(pet_2.name)
+    expect(page).to_not have_content(pet_1.name)
 
   end
 
