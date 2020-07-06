c638a8b7f082762fa29063ef3150620f1fefebb5
Ashkan Abbasi <airashkan@yahoo.com>
2020-07-05 21:21:43 -0600

    Add functionality to be able to edit a shelter from shelter index page

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Shelters Index Page" do
-  before :each do
-    @shelter1 = Shelter.create!(name: "Pups For You", address: "1808 Pup lane", city: "Denver", state: "Colorado", zip: "80027")
-    @shelter2 = Shelter.create!(name: "Kitty Rescue", address: "1505 Kitty Dr.", city: "Denver", state: "Colorado", zip: "80025")
-
-  end
 
   it "can see a list of names of all shelters" do
+    shelter1 = Shelter.create!(name: "Pups For You", address: "1808 Pup lane", city: "Denver", state: "Colorado", zip: "80027")
+
+    shelter2 = Shelter.create!(name: "Kitty Rescue", address: "1505 Kitty Dr.", city: "Denver", state: "Colorado", zip: "80025")
+
+
     visit "/shelters"
 
-    expect(page).to have_content(@shelter1.name)
-    expect(page).to have_content(@shelter2.name)
+    expect(page).to have_content(shelter1.name)
+    expect(page).to have_content(shelter2.name)
   end
 
   it "see a link that takes you to a form to create a new shelter" do
@@ -32,6 +32,30 @@ RSpec.describe "Shelters Index Page" do
     expect(current_path).to eq('/shelters')
 
     expect(page).to have_content("Puppy Love")
+  end
+
+  it "has links next to each shelter to edit that shelter" do
+    shelter1 = Shelter.create!(name: "Pups For You", address: "1808 Pup lane", city: "Denver", state: "Colorado", zip: "80027")
+
+    visit '/shelters'
+
+    click_on 'Update Shelter'
+
+    expect(current_path).to eq("/shelters/#{shelter1.id}/edit")
+
+    fill_in "name", with: "Pups For You"
+    fill_in "address", with: "1600 Puppy Blvd"
+    fill_in "city", with: "Littleton"
+    fill_in "state", with: "CO"
+    fill_in "zip", with: "80235"
+    click_on "Update Shelter"
+
+    expect(current_path).to eq("/shelters/#{shelter1.id}")
+
+    expect(page).to have_content("1600 Puppy Blvd")
+    expect(page).to have_content("Littleton")
+    expect(page).to have_content("80235")
+    expect(page).to_not have_content("1808 Pup lane")
 
   end
 end
