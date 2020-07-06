67de1c7654335191a9edb346e9d6240b9a45b017
Ashkan Abbasi <airashkan@yahoo.com>
2020-07-05 23:48:35 -0600

    Add links for shelter and pet index to nav bar and add color to page

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ Shelter.destroy_all
                       description: "Sweet senior dog",
                       approximate_age: "18",
                       sex: "Male",
-                      adoption_status: "adoptable",
+                      status: "adoptable",
                       shelter_id: @shelter_1.id)
 
 
@@ -38,7 +38,7 @@ Shelter.destroy_all
                       description: "Very good with kids and other animals",
                       approximate_age: "6",
                       sex: "Male",
-                      adoption_status: "adoptable",
+                      status: "adoptable",
                       shelter_id: @shelter_1.id)
 
 @pet_3 = Pet.create(  image: "https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2019/02/dog-451643.jpg?h=bf654dbc&itok=MQGvBmuo",
@@ -46,7 +46,7 @@ Shelter.destroy_all
                       description: "Adopt me I'm a cuddle bug",
                       approximate_age: "2",
                       sex: "Female",
-                      adoption_status: "adoptable",
+                      status: "adoptable",
                       shelter_id: @shelter_2.id)
 
 @pet_4 = Pet.create(  image: "https://d1941uuft27pfg.cloudfront.net/breed-uploads/2018/08/akita-card-medium.jpg?bust=1535569354",
@@ -54,7 +54,7 @@ Shelter.destroy_all
                       description: "Stays to herself except when you have treats",
                       approximate_age: "4",
                       sex: "Female",
-                      adoption_status: "adoptable",
+                      status: "adoptable",
                       shelter_id: @shelter_4.id)
 
 @pet_5 = Pet.create(  image: "https://cdn.mos.cms.futurecdn.net/QjuZKXnkLQgsYsL98uhL9X.jpg",
@@ -62,7 +62,7 @@ Shelter.destroy_all
                       description: "Very energetic",
                       approximate_age: "4",
                       sex: "Male",
-                      adoption_status: "adoptable",
+                      status: "adoptable",
                       shelter_id: @shelter_3.id)
 
 @pet_6 = Pet.create(  image: "https://i0.wp.com/cdn-prod.medicalnewstoday.com/content/images/articles/327/327387/cat-playing-with-toy.jpg?w=1155&h=1734",
@@ -70,7 +70,7 @@ Shelter.destroy_all
                       description: "Sweet and loving",
                       approximate_age: "8",
                       sex: "Female",
-                      adoption_status: "adoptable",
+                      status: "adoptable",
                       shelter_id: @shelter_2.id)
 
 @pet_7 = Pet.create(  image: "https://images.immediate.co.uk/production/volatile/sites/4/2018/12/GettyImages-918529586-35c7227.jpg?quality=90&crop=61px%2C468px%2C7710px%2C3317px&resize=960%2C408",
@@ -78,7 +78,7 @@ Shelter.destroy_all
                       description: "Give me a chance",
                       approximate_age: "3",
                       sex: "Male",
-                      adoption_status: "adoptable",
+                      status: "adoptable",
                       shelter_id: @shelter_3.id)
 
 @pet_9 = Pet.create(  image: "https://dcist.com/wp-content/uploads/sites/3/2020/02/wilford_newsletter.jpg",
@@ -86,5 +86,5 @@ Shelter.destroy_all
                       description: "I'm just big boned",
                       approximate_age: "5",
                       sex: "Female",
-                      adoption_status: "adoptable",
+                      status: "adoptable",
                       shelter_id: @shelter_1.id)
